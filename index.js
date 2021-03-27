57f1f0da0f0d364980c5a8f1d23ed830129fe87a
taciturnZ <realdetay@gmail.com>
2021-03-27 13:48:06 +0300

    edit css and js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const input = document.querySelector("input");
 const button = document.querySelector(".add");
 const todoList = document.querySelector(".todoList");
-document.addEventListener('DOMContentLoaded', loadTodos)
-document.querySelector('#todos').addEventListener('change', todoFilter)
-button.addEventListener('click', addTodo);
+document.addEventListener("DOMContentLoaded", loadTodos);
+document.querySelector("#todos").addEventListener("change", todoFilter);
+button.addEventListener("click", addTodo);
+window.addEventListener("keydown", function (e) {
+  if (e.key == "Enter") {
+    addTodo();
+  }
+});
 
 function addTodo() {
-  if(!!input.value){
+  if (!!input.value) {
     const div = document.createElement("div");
     const li = document.createElement("li");
     const deleteButton = document.createElement("i");
@@ -29,71 +34,86 @@ function addTodo() {
     div.appendChild(doneButton);
     li.textContent = input.value;
 
-    saveLocalStore(input.value)
+    saveLocalStore(input.value);
     input.value = null;
 
     const delTodo = document.querySelectorAll(".del");
     delTodo.forEach((button) => button.addEventListener("click", deleteTodo));
 
-    const doneTodo = document.querySelectorAll('.done');
-    doneTodo.forEach((button) => button.addEventListener('click', dashedTodo));
+    const doneTodo = document.querySelectorAll(".done");
+    doneTodo.forEach((button) => button.addEventListener("click", dashedTodo));
   }
 }
 
-function todoFilter(){
-  const todos = document.querySelectorAll('.todoDiv')
-  const value = this.value
+function todoFilter() {
+  const todos = document.querySelectorAll(".todoDiv");
+  const value = this.value;
   todos.forEach((todo) => {
-
-    switch(value){
-      case 'all': {
-        todo.style.display = 'flex'
+    switch (value) {
+      case "all":
+        {
+          todo.style.display = "flex";
+        }
+        break;
+      case "done": {
+        if (todo.classList.contains("remove")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
       }
-      break
-      case 'done': {
-        if(todo.classList.contains('remove')){
-          todo.style.display = 'flex'
-        }
-        else{
-          todo.style.display = 'none'
-        }
-        break
-      }
-      case 'notDone': {
-        if(!todo.classList.contains('remove')){
-          todo.style.display = 'flex'
-        }
-        else{
-          todo.style.display = 'none'
+      case "notDone": {
+        if (!todo.classList.contains("remove")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
         }
       }
     }
-  })
+  });
 }
 
-function saveLocalStore(savedTodo){
+function saveLocalStore(todo) {
   let todos;
-  if(!window.localStorage.todos){
-    todos = []
+  if (!window.localStorage.todos) {
+    todos = [];
+  } else {
+    todos = JSON.parse(window.localStorage.todos);
   }
-  else {
-    todos = JSON.parse(window.localStorage.todos)
+  todos.push(todo);
+  window.localStorage.todos = JSON.stringify(todos);
+}
+
+function saveDashed(todo) {
+  let todos;
+  if (!window.localStorage.dashed) {
+    todos = [];
+  } else {
+    todos = JSON.parse(window.localStorage.dashed);
   }
-  todos.push(savedTodo)
-  window.localStorage.todos = JSON.stringify(todos)
+  todos.push(todo);
+  window.localStorage.dashed = JSON.stringify(todos);
 }
 
 function delFromLocalStore(todo) {
-  const theTodo = todo
-  const todolist = JSON.parse(window.localStorage.todos)
-  const deleteTodo = todolist.filter(todo => todo !== theTodo)
-  window.localStorage.todos = JSON.stringify(deleteTodo)
+  const theTodo = todo;
+  const todolist = JSON.parse(window.localStorage.todos);
+  const deleteTodo = todolist.filter((todo) => todo !== theTodo);
+  window.localStorage.todos = JSON.stringify(deleteTodo);
 }
 
-function loadTodos () {
-  if(window.localStorage.todos){
-    const todos = JSON.parse(window.localStorage.todos)
-    todos.forEach(todo => {
+function delDashed(todo) {
+  const theTodo = todo;
+  const todolist = JSON.parse(window.localStorage.dashed);
+  const deleteTodo = todolist.filter((todo) => todo !== theTodo);
+  window.localStorage.dashed = JSON.stringify(deleteTodo);
+}
+
+function loadTodos() {
+  if (window.localStorage.todos) {
+    const todos = JSON.parse(window.localStorage.todos);
+    todos.forEach((todo) => {
       const div = document.createElement("div");
       const li = document.createElement("li");
       const deleteButton = document.createElement("i");
@@ -114,29 +134,33 @@ function loadTodos () {
       div.appendChild(li);
       div.appendChild(deleteButton);
       div.appendChild(doneButton);
-      li.textContent = todo
-      window.localStorage.todos = JSON.stringify(todos)
+      li.textContent = todo;
+      window.localStorage.todos = JSON.stringify(todos);
 
       const delTodo = document.querySelectorAll(".del");
       delTodo.forEach((button) => button.addEventListener("click", deleteTodo));
-  
-      const doneTodo = document.querySelectorAll('.done');
-      doneTodo.forEach((button) => button.addEventListener('click', dashedTodo));
-    })
+
+      const doneTodo = document.querySelectorAll(".done");
+      doneTodo.forEach((button) =>
+        button.addEventListener("click", dashedTodo)
+      );
+    });
   }
 }
-
+function loadDashed() {}
 function deleteTodo() {
   const todo = this.parentElement;
-  todo.remove()
-  delFromLocalStore(this.parentElement.textContent)
+  todo.remove();
+  delFromLocalStore(this.parentElement.textContent);
 }
 
 function dashedTodo() {
-  const todos = this.parentElement
-  todos.classList.toggle('remove')
-  delFromLocalStore(this.parentElement.textContent)
-  if(todos.classList.value == 'todoDiv'){
-    saveLocalStore(this.parentElement.textContent)
+  const todos = this.parentElement;
+  todos.classList.toggle("remove");
+  delFromLocalStore(this.parentElement.textContent);
+  saveDashed(this.parentElement.textContent);
+  if (todos.classList.value == "todoDiv") {
+    saveLocalStore(this.parentElement.textContent);
+    delDashed(this.parentElement.textContent);
   }
 }
